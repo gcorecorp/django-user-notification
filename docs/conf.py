5a21b81c47892639bd32143baba0eddbbd9062ad
aiden <aiden_lu@wochacha.com>
2022-08-01 13:37:41 +0800

    docs: fix package name

--- docs/conf.py
+++ docs/conf.py
@@ -100,7 +100,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'notificationdoc'
+htmlhelp_basename = 'djangousernotificationdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------
